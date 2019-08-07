b3b37c966c0ac9c21624aa4c1c10c4ad75db6778
Miller Vaughan <millvaug@umich.edu>
2019-08-07 11:23:38 -0400

    shadow done

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,18 @@ function toggleNavbar() {
     navRight.attr('class', 'navbar-right');
   }
 }
+
+$(document).scroll(function() {
+  if ($(window).scrollTop() === 0) {
+    $('.navbar').css({"-moz-box-shadow": "0px 0px",
+                      "-webkit-box-shadow": "0px 0px",
+                      "box-shadow": "0px 0px"});
+    console.log("top!");
+  }
+  if ($(window).scrollTop() !== 0) {
+    $('.navbar').css({"-moz-box-shadow": "0 8px 6px -6px",
+                      "-webkit-box-shadow": "0 8px 6px -6px",
+                      "box-shadow": "0 8px 6px -6px"});
+    console.log("not top!");
+  }
+})
