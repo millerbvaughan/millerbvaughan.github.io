8ded7b39901bc228c39140bdfe612c40dc586ba0
Miller Vaughan <millvaug@umich.edu>
2019-08-07 15:27:25 -0400

    slower fade in/out

--- scripts/script.js
+++ scripts/script.js
@@ -13,12 +13,21 @@ $(document).scroll(function() {
     $('.navbar').css({"-moz-box-shadow": "0px 0px",
                       "-webkit-box-shadow": "0px 0px",
                       "box-shadow": "0px 0px"});
-    console.log("top!");
   }
-  if ($(window).scrollTop() !== 0) {
-    $('.navbar').css({"-moz-box-shadow": "0 8px 6px -6px",
-                      "-webkit-box-shadow": "0 8px 6px -6px",
-                      "box-shadow": "0 8px 6px -6px"});
-    console.log("not top!");
+  if ($(window).scrollTop() > 5) {
+    $('.navbar').css({"-moz-box-shadow": "0 8px 6px -6px rgba(0,0,0,0.1)",
+                      "-webkit-box-shadow": "0 8px 6px -6px rgba(0,0,0,0.1)",
+                      "box-shadow": "0 8px 6px -6px rgba(0,0,0,0.1)"});
+  }
+  if ($(window).scrollTop() > 10) {
+    $('.navbar').css({"-moz-box-shadow": "0 8px 6px -6px rgba(0,0,0,0.2)",
+                      "-webkit-box-shadow": "0 8px 6px -6px rgba(0,0,0,0.2)",
+                      "box-shadow": "0 8px 6px -6px rgba(0,0,0,0.2)"});
+  }
+
+  if ($(window).scrollTop() > 15) {
+    $('.navbar').css({"-moz-box-shadow": "0 8px 6px -6px rgba(0,0,0,0.3)",
+                      "-webkit-box-shadow": "0 8px 6px -6px rgba(0,0,0,0.3)",
+                      "box-shadow": "0 8px 6px -6px rgba(0,0,0,0.3)"});
   }
 })
